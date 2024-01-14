42ab00a902eaeccd1abd476e57eb6deda5e4a37a
Masekela Lerato <leratomasekela234@gmail.com>
2024-01-15 00:23:31 +0200

    working on the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-return(
-  <>
-  <Login/>
-  </>
-)
+  return (
+    <div className='text-white h-[100vh] flex justify-center bg-cover' style={{ backgroundColor: 'red' }}>
+      <Routes>
+        <Route path='login' element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
 
-function Login(){
-    return
+function Login() {
+  return (
+    <div>
+      <div>
+        <h1>Login</h1>
+        <form action='submit'>
+          <div>
+            <input type='email' />
+            <label htmlFor=''>Your email</label>
+          </div>
+          <div>
+            <input type='email' />
+            <label htmlFor=''>Your email</label>
+          </div>
+          <div>
+            <input type='checkbox' name='' id='' />
+            <label htmlFor='Remember Me'></label>
+          </div>
+          <span>Forgot Password?</span>
+          <button type='Submit'>Login</button>
+         
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default Login;
